afb4bd5614aa75ec3587fdbce6e329a5d0acaaf5
Tinyunknown <106500276+Tinyunknown@users.noreply.github.com>
2022-12-22 15:48:57 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,12 @@
+Hello! This is a small little script I made with pyqutogui that automatically
+signs you into Final Fantasy 14 Online if your to lazy(like I am) to input your
+password.
+
+If the program doesn't work, make sure you have the script and the images in the same
+folder. you might also need pyautogui and pillow installed. You also might need to
+move the FFXIV window so your command prompt doesn't cover it.
+
+I have also included a .exe file of the script that, hopefully, will make it so you
+don't need the modules installed.
+
+Created by Ligig 
---
+++ autoPassword.py
@@ -0,0 +1,20 @@
+#imports pyautogui for use
+import pyautogui
+
+#sets up variables for your password
+password = "pNAs7GEG2eLrs9T"
+
+#finds where the password box is and stores it in a variable
+if pyautogui.locateCenterOnScreen('password.png') == pyautogui.ImageNotFoundException():
+	x, y = pyautogui.locateCenterOnScreen('password2.png')
+else:
+	x, y = pyautogui.locateCenterOnScreen('password.png')
+
+#moves the mouse to the password box, clicks it, and enters the password
+pyautogui.moveTo(x, y)
+pyautogui.click()
+pyautogui.write('pNAs7GEG2eLrs9T')
+
+#gets the coords for the login button, then clicks it
+x, y = pyautogui.locateCenterOnScreen('login.png')
+pyautogui.click(x, y)
